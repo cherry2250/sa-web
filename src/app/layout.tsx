ba6ce526732e7ea7f8e9960ef2f616e186e71c3a
cherry2250 <kcr7812@naver.com>
2025-07-15 18:11:26 +0900

    fix : use client bug fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "@/widgets/sidebar/ui/Sidebar";
 import { Header } from "@/widgets/header/ui/Header";
-import { useEffect } from "react";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,15 +24,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
-  useEffect(() => {
-    const setVh = () => {
-      document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
-    };
-    setVh();
-    window.addEventListener('resize', setVh);
-    return () => window.removeEventListener('resize', setVh);
-  }, []);
   return (
     <html lang="en">
       <body
